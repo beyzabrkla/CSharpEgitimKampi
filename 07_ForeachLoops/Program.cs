71859ab6f1d5362860f7efdb1235b98ff5f148b1
Beyza Rabia Kılıçaslan <beyzarabia121@gmail.com>
2025-10-06 17:19:41 +0300

    6/10/2025

---
+++ 07_ForeachLoops/Program.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ForeachLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Foreach Döngüsü
+
+            //foreach (1;2;3;4) 4 parametreli bir döngüdür
+            //1: Değişken Türü
+            //2:Değişken Adı
+            //3:In
+            //4:Liste, Koleksiyon, Dizi
+
+
+            /*
+            string[] cities = {"milano","roma","budapeşte","ankara","istanbul","varşova" };
+
+            foreach (string x in cities) 
+            {
+                Console.WriteLine(x);
+            }
+            */
+
+
+            /*
+            int[] numbers = {45,78,985,635,74,11,22,33,41,205,6578,10394 };
+
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            */
+
+
+            /*
+            int[] numbers = { 45, 78, 985, 635, 74, 11, 22, 33, 41, 205, 6578, 10394 };
+
+            foreach (int number in numbers)
+            {
+                if (number %2 ==0)
+                {
+                    Console.WriteLine(number);
+                }    
+            }    
+            */
+
+
+            /*
+            int[] numbers = { 45, 78, 985, 635, 74, 11, 22, 33, 41, 205, 6578, 10394 };
+
+            int total = 0;
+
+            foreach (int i in numbers)
+            {
+                total += i; 
+            }
+            Console.WriteLine(total);
+            */
+
+
+            /*
+            List<int> numbers = new List<int>()
+            {
+                1,2,3,4,5,8
+            };
+
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            */
+
+
+            /*
+            string word = "Merhaba";
+
+            foreach (char c in word)
+            {
+                Console.WriteLine(c);
+            }
+            */
+
+            #endregion
+
+            #region Örnek Sınav Sistemi Uygulaması
+            /*
+            Console.Write("***** C# Eğitim Kampı Sınav Uygulaması *****");
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            //sınıftaki öğrenci sayısını kullanıcıdan alma
+            Console.WriteLine("--------------------------------------");
+
+            Console.WriteLine("Sınıfınızda kaç öğrenci var ? ");
+
+            int studentCount =int.Parse(Console.ReadLine());
+            Console.WriteLine("--------------------------------------");
+
+            //öğrenci isimlerini ve not ortalamalarını saklayacak diziler
+            string[] studentNames= new string[studentCount];
+            double[] studentExamAvg = new double[studentCount];
+
+
+            for (int i = 0; i < studentCount; i++)
+            {
+                Console.Write($"{i + 1}. öğrencinin ismini giriniz:");
+                studentNames[i] = Console.ReadLine();
+
+                double totalExamResult = 0;
+
+                //her öğrenci için 3 sınav notu girişi
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.WriteLine($"{studentNames[i]} isimli öğrencinin {j + 1}. sınav notunu giriniz: ");
+                    double value = double.Parse(Console.ReadLine());
+                    totalExamResult += value; //notları topluyoruz
+
+                }
+
+                Console.WriteLine();
+
+                studentExamAvg[i] = totalExamResult / 3; //ortalama
+            }
+
+            //sınav ortalamarı
+            for (int i = 0; i< studentCount; i++)
+            {
+                Console.WriteLine("---------------------------------------------");
+
+                Console.WriteLine($"{studentNames[i]} adlı öğrencinin ortalaması: {studentExamAvg[i]}");
+
+
+                //öğrencilerin ortalaması ve geçip kalma durumları
+                if (studentExamAvg[i] >= 50)
+                {
+                    Console.WriteLine($"{studentNames[i]} adlı öğrenci dersi geçti");
+                }
+                else
+                {
+                    Console.WriteLine($"{studentNames[i]} adlı öğrenci dersten kaldı");
+                }
+                Console.WriteLine("---------------------------------------------");
+
+            }
+
+            */
+            #endregion
+
+
+            Console.Read();
+        
+        }
+    }
+}
---
+++ 09_DatabasePeoject/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_DatabasePeoject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ado.net   C# programlama dilinde sql yapılarını kullanmamızı sağlayan bir çerçeve
+
+            Console.WriteLine("***** C# Veri Tabanlı Ürün-Kategori Bilgi Sistemi *****");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            string tableNumber; 
+
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("1-Kategoriler");
+            Console.WriteLine("2-Ürünler");
+            Console.WriteLine("3-Siparişler");
+            Console.WriteLine("4-Çıkış Yap");
+            Console.Write("Lütfen getirmek istediğiniz tablo numarasını giriniz: ");
+           
+            tableNumber = Console.ReadLine();
+            
+            Console.WriteLine("-------------------------------");
+
+
+            SqlConnection connection = new SqlConnection("Data Source=BEYZA\\BEYZA_DEV; Initial Catalog=EgitimKampiDb; integrated security=true;");
+            //sınıf        nesne         
+            connection.Open(); //bağlantı açıldı
+           
+            SqlCommand command = new SqlCommand("Select * From TblCategory", connection); //yazılan sql sorgusunun düzgün çalışması için bağladığımız db nin adresini çağırdık.
+            SqlDataAdapter adapter = new SqlDataAdapter(command); //sql dataadapter= c# kodlarıyla sql sunucusu arasında köprü görevi görür
+            DataTable dataTable = new DataTable(); //DataTable verileri ram belleğe almasını sağlayacak
+            adapter.Fill(dataTable); 
+
+            connection.Close(); //bağlantı kapatıldı.
+
+            foreach (DataRow row in dataTable.Rows) 
+            {
+                foreach (var item in row.ItemArray) //satırdan gelen dizinin içeriğini okumak 
+                {
+                    Console.Write(item.ToString());
+                }
+                Console.WriteLine();
+            }
+
+            Console.Read();
+        }
+    }
+}
